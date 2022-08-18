c074eed084420fc6548dbf21c73685bd4a18994d
Sergio Lissanou <sergitodelavega@gmail.com>
2022-08-18 09:57:30 +0100

    Installation of the bootstrap template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Anyar Bootstrap Template - Index</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="{{ asset('/img/favicon/favicon.png') }}" rel="icon">
+    <link href="{{ asset('/img/favicon/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('/css/front/vendor/animate.css/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/front/vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/front/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/front/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/front/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/front/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/front/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/front/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('css/front/style.css') }}" rel="stylesheet">
+    
+    
+</head>
+<body>
+    <!-- header -->
+    <nav class="nav navbar-dark bg-primary-blue navbar-expand-lg">
+        <div class="navbar-brand">
+            <a href="/" class="topbar-logo router-link-active" target="_self">
+                <img src="" alt="">
+            </a>
+        </div>
+        <button type="button" aria-label="Toggle navigation" class="navbar-toggler collapsed" aria-expanded="false" aria-controls="nav-collapse" style="overflow-anchor: none;"></button>
+        <div id="nav-collapse" class="navbar-collapse collapse" style="display: none;">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="#" aria-current="page" class="nav-link router-link-exact-active router-link-active" target="_self">Menu 1</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" aria-current="page" class="nav-link router-link-exact-active router-link-active" target="_self">Menu 2</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" aria-current="page" class="nav-link router-link-exact-active router-link-active" target="_self">Menu 3</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
